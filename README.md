1e157ffa8d53d7a8c948ba2db3e953f72f78f9c1
Sven Nilsen <sven@cutoutpro.com>
2012-10-16 09:11:07 +0200

    First commit

--- README.md
+++ README.md
@@ -1,4 +1,76 @@
-luagroups
-=========
+luagroups - Group oriented programming in Lua
 
-Group oriented programming in Lua
+Chan - Concurrency in C
+BSD license.
+by Sven Nilsen, 2012
+http://www.cutoutpro.com
+
+Version: 0.000 in angular degrees version notation
+http://isprogrammingeasy.blogspot.no/2012/08/angular-degrees-versioning-notation.html
+
+
+## How To Use It
+
+    require "groups.lua"
+
+## What Is Group Oriented Programming?
+
+Group Oriented Programming is a paradigm that uses Boolean Algebra On Groups to compute the indices before a for-loop rather than using lots of ifs and "continue" statements. It can also give a boost in overall performance.
+
+The reason it can give performance gain is that behind the scenes it uses a special representation of the groups that require fewer operations when "similar data" is neighbors. This is called a "group bitstream".
+
+Here is an example:
+
+    -- First we need some data to calculate with.
+    -- Notice that Clark has no hair member.
+    local people = {{name="John", hair=true}, {name="Clark"}}
+
+    -- We can extract the groups we need on the fly.
+    local name = groups_HasKey(people, "name")
+    local hair = groups_HasKey(people, "hair")
+
+    -- Using operator overloading, we combine groups
+    -- and: * or: + except: -
+    local name_and_hair = name * hair
+
+    -- Iterate through the indices using the group(t) iterator.
+    -- This should print out only "John".
+    local person
+    for i in group(name_and_hair) do
+        -- indices start with 0 in a group but 1 in Lua.
+        person = people[i+1]
+        print(person.name)
+    end
+
+Group Oriented Programming spans over any Boolean Algebra On Groups. It can be implemented with raw bitstream where each bit correspond to an index. This works better for high entropy where the average interval between change is less than 2*64 (64 is number of bits in a double).
+
+Group bitstream is easier to read for humans since it is just a list of ranges ex. [2,4, 6,8, 10,13] where even number mark the start and odd the end. Unlike raw bitstream it also can handle continuous spaces.
+
+One area of research is particular responsible for the invention of group oriented programming. Bitstreams associates properties with number indices. This makes lot of number theory directly applicable. It makes it possible to study the properties of a large space of objects without needing a specific application. Later one can use the data achieved from this to study other problems, using the algebra to calculate solutions.
+
+## Why Use Group Oriented Programming?
+
+Here are 4 top reasons:
+
+1. Preprocessing. For N items, algebra has worst case O(N) but average avg(&lt; N). Which means it speeds up program in many cases.
+2. Better code reuse as caused by the generic nature of groups.
+3. Easier to solve complex problems step by step and makes debugging easier.
+4. Does not require database and SQL, runs a lot faster.
+
+## Writing Your Own Stuff
+
+Because there is a lot of generic algorithms in group oriented programming, I decided to write only one function in this library. It will serve as a prototype for your own custom functions. This way it will be easier to maintain this library without having people depending on it. The function returns a group of all items in an array that has a property.
+
+    <group> groups_HasKey(<a (table)>, <prop (string)>, <region (group, optional)>)
+
+The region to search within the array is optional, but is faster since it does not iterate through the whole array.
+
+    -- A bit slower.
+    local US = groups_HasKey(people, "US")
+    local name = groups_HasKey(people, "name")
+    local name_and_in_US = US * name
+
+    -- A bit faster.
+    local US = groups_HasKey(people, "US")
+    local name_and_in_US = groups_HasKey(people, "name", US)
+
---
+++ groups.lua
@@ -0,0 +1,340 @@
+--[[
+
+luagroups - Group oriented programming in Lua.
+
+Chan - Concurrency in C
+BSD license.
+by Sven Nilsen, 2012
+http://www.cutoutpro.com
+
+Version: 0.000 in angular degrees version notation
+http://isprogrammingeasy.blogspot.no/2012/08/angular-degrees-versioning-notation.html
+
+--]]
+
+--[[
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+1. Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+The views and conclusions contained in the software and documentation are those
+of the authors and should not be interpreted as representing official policies,
+either expressed or implied, of the FreeBSD Project.
+--]]
+
+-- Takes group bitstream OR operation between two groups.
+function groups_Or(a, b)
+	local list = {};
+	
+	if #a == 0 and #b == 0 then return list end
+	if #a == 0 then
+    for k = 0, #b-1 do list[k+1] = b[k+1] end
+    
+		return list
+	end
+	if #b == 0 then
+    for k = 0, #a-1 do list[k+1] = a[k+1] end
+		
+    return list
+	end
+	
+  local i, j, iO, jO, pa, pb = 0, 0, 0, 0, 0, 0
+	local ba, bb, oldB = false, false, false
+	while i < #a or j < #b do
+    if i >= #a then iO = #a-1 else iO = i end
+    if j >= #b then jO = #b-1 else jO = j end
+    
+		pa, pb = a[iO+1], b[jO+1]
+		
+		if pa == pb then
+			ba = not ba; bb = not bb
+			if ((ba or bb) ~= oldB) then list[#list+1] = pa end
+			
+      i, j = i+1, j+1
+		elseif (pa < pb or j >= #b) and i < #a then
+			ba = not ba
+			if (ba or bb) ~= oldB then list[#list+1] = pa end
+      
+			i = i + 1
+		elseif j < #b then
+			bb = not bb
+			if (ba or bb) ~= oldB then list[#list+1] = pb end
+      
+			j = j + 1
+		else
+			break
+    end
+		
+		oldB = ba or bb
+	end -- while loop.
+	
+	return list
+end
+
+-- Union intersection of groups.
+function groups_And(a, b)
+	local list = {};
+	
+	if #a == 0 or #b == 0 then return list end
+	
+  local i, j, iO, jO, pa, pb = 0, 0, 0, 0, 0, 0
+	local ba, bb, oldB = false, false, false
+	while i < #a or j < #b do
+    if i >= #a then iO = #a-1 else iO = i end
+    if j >= #b then jO = #b-1 else jO = j end
+    
+		pa, pb = a[iO+1], b[jO+1]
+		
+		if pa == pb then
+			ba = not ba; bb = not bb
+			if ((ba and bb) ~= oldB) then list[#list+1] = pa end
+			
+      i, j = i+1, j+1
+		elseif (pa < pb or j >= #b) and i < #a then
+			ba = not ba
+			if (ba and bb) ~= oldB then list[#list+1] = pa end
+      
+			i = i + 1
+		elseif j < #b then
+			bb = not bb
+			if (ba and bb) ~= oldB then list[#list+1] = pb end
+      
+			j = j + 1
+		else
+			break
+    end
+		
+		oldB = ba and bb
+	end -- while loop.
+	
+	return list
+end
+
+-- Subtracts one group from another.
+function groups_Except(a, b)
+	local list = {};
+	
+	if #a == 0 then return list end
+	
+  local i, j, iO, jO, pa, pb = 0, 0, 0, 0, 0, 0
+	local ba, bb, oldB = false, true, false
+	while i < #a or j < #b do
+    if i >= #a then iO = #a-1 else iO = i end
+    if j >= #b then jO = #b-1 else jO = j end
+    
+		pa, pb = a[iO+1], b[jO+1]
+		
+		if pa == pb then
+			ba = not ba; bb = not bb
+			if ((ba and bb) ~= oldB) then list[#list+1] = pa end
+			
+      i, j = i+1, j+1
+		elseif (pa < pb or j >= #b) and i < #a then
+			ba = not ba
+			if (ba and bb) ~= oldB then list[#list+1] = pa end
+      
+			i = i + 1
+		elseif j < #b then
+			bb = not bb
+			if (ba and bb) ~= oldB then list[#list+1] = pb end
+      
+			j = j + 1
+		else
+			break
+    end
+		
+		oldB = ba and bb
+	end -- while loop.
+	
+	return list
+end
+
+group_bitstream = {__mul = groups_And, __add = groups_Or, __sub = groups_Except}
+
+-- Iterator for for loops.
+function group(t)
+  local i, j = 0, -1
+  local n = table.getn(t)
+  local stop = 0
+  return function ()
+    j = j + 1
+    if j >= stop then 
+      if i >= n then return end
+      j = t[i+1]
+      stop = t[i+2]
+      i = i + 2
+    end
+    return j
+  end
+end
+
+-- Creates a group of items in array that have a specific property.
+-- a = the array to check items.
+-- prop = the field to look for.
+-- region (optional) = a group to limit the search within the array.
+-- Use this function as template for custom group generation.
+function groups_HasKey(a, prop, region)
+  local list = {}
+  setmetatable(list, group_bitstream)
+  
+  if region ~= nil then
+    local had, has = false, false
+    local j = -1
+    for i in group(region) do
+      -- Check for jumping over ranges, add correction.
+      if had and j ~= -1 and i-j > 1 then
+        list[#list+1] = j+1
+        had = false
+      end
+      j = i
+      
+      -- Condition that evaluates to true.
+      has = a[i+1] ~= nil and a[i+1][prop] ~= nil
+      -- End condition.
+      
+      if has ~= had then
+        list[#list+1] = i
+      end
+      had = has
+    end
+    if has then list[#list+1] = j + 1 end
+  else
+    local had, has = false, false
+    for i = 0, #a-1 do
+      -- Condition that evaluates to true.
+      has = a[i+1] ~= nil and a[i+1][prop] ~= nil
+      -- End condition.
+      
+      if has ~= had then
+        list[#list+1] = i
+      end
+      had = has
+    end
+    if has then list[#list+1] = #a end
+  end
+  
+  return list
+end
+
+function test_groups_Or_1()
+  local a = {2, 4}
+  local b = {0, 10}
+  local c = groups_Or(a, b)
+  
+  assert(#c == 2)
+  assert(c[1] == 0)
+  assert(c[2] == 10)
+end
+
+function test_groups_Or_2()
+  local a = {2, 4}
+  local b = {4, 6}
+  local c = groups_Or(a, b)
+  
+  assert(#c == 2)
+  assert(c[1] == 2)
+  assert(c[2] == 6)
+end
+
+function test_groups_And_1()
+  local a = {2, 4}
+  local b = {3, 5}
+  local c = groups_And(a, b)
+  
+  assert(#c == 2)
+  assert(c[1] == 3)
+  assert(c[2] == 4)
+end
+
+function test_groups_Except_1()
+  local a = {2, 4}
+  local b = {3, 5}
+  local c = groups_Except(a, b)
+  
+  assert(#c == 2)
+  assert(c[1] == 2)
+  assert(c[2] == 3)
+end
+
+function test_groups_Except_2()
+  local a = {2, 8}
+  local b = {3, 5}
+  local c = groups_Except(a, b)
+  
+  assert(#c == 4)
+  assert(c[1] == 2)
+  assert(c[2] == 3)
+  assert(c[3] == 5)
+  assert(c[4] == 8)
+end
+
+function test_groups_HasKey_1()
+  local a = {{firstName = "Bill", lastName = "Smith"}, {firstName = "Malcolm"}}
+  local b = groups_HasKey(a, "firstName")
+  local c = groups_HasKey(a, "lastName")
+  
+  assert(#b == 2, #b)
+  assert(#c == 2, #c)
+  assert(b[1] == 0)
+  assert(b[2] == 2, b[2])
+  assert(c[1] == 0, c[1])
+  assert(c[2] == 1)
+end
+
+function test_groups_HasKey_2()
+  local a = {{x=0,y=0}}
+  local xy = groups_HasKey(a, "y", groups_HasKey(a, "x"))
+  
+  assert(#xy == 2, #xy)
+  assert(xy[1] == 0)
+  assert(xy[2] == 1)
+end
+
+function test_groups_HasKey_3()
+  local a = {{x=0,y=0}, {x=0,y=0,z=0}}
+  local z = groups_HasKey(a, "z")
+  local xz = groups_HasKey(a, "x", z)
+  
+  assert(#xz == 2, #xz)
+  assert(xz[1] == 1)
+  assert(xz[2] == 2)
+end
+
+function test_groups_HasKey_4()
+  local a = {{x=0,y=0}, {x=0,y=0,z=0}, {x=0,y=0}, {x=0,y=0,z=0}, {x=0,y=0}, {x=0,y=0,z=0}}
+  local z = groups_HasKey(a, "z")
+  local xz = groups_HasKey(a, "x", z)
+  
+  assert(#xz == 6, #xz)
+  assert(xz[1] == 1)
+  assert(xz[2] == 2)
+  assert(xz[3] == 3)
+  assert(xz[4] == 4)
+  assert(xz[5] == 5)
+  assert(xz[6] == 6)
+end
+
+function test_groups()
+  test_groups_Or_1()
+  test_groups_Or_2()
+  test_groups_And_1()
+  test_groups_Except_1()
+  test_groups_Except_2()
+  test_groups_HasKey_1()
+  test_groups_HasKey_2()
+  test_groups_HasKey_3()
+  test_groups_HasKey_4()
+end
